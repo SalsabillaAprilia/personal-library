9dc18074968837b033e83925827baf4fd9fed028
Salsabilla Aprilia <salsabillaapril05@gmail.com>
2025-08-04 14:49:46 +0700

    ubah cors

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,14 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "https://superlative-griffin-405691.netlify.app",
+  credentials: true
+}));
+app.options("*", cors({
+  origin: "https://superlative-griffin-405691.netlify.app",
+  credentials: true
+}));
 app.use(express.json());
 
 // Routes
